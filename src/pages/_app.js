92f91590695c322fe5cc8cf4eb5960c6e0f45998
groverjatin17 <groverjatin17@gmail.com>
2024-04-04 17:29:34 -0400

    fixed the icon for tab

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>ItFrog</title>
-        <link rel="icon" href="/img/logo-white.ico" />
+        <link rel="icon" href="/img/frog.ico" />
       </Head>
       {/* <Cursor /> */}
       <LoadingScreen />
